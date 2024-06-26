7501b3b73a7873642f103ebcfe977b1c62b47e31
SimanyeSTS <simanyesomdaka@gmail.com>
2024-06-26 08:27:46 +0200

    sructureSet

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/resume">Resume</router-link> |
+    <router-link to="/education">Education</router-link> |
+    <router-link to="/experiences">Experiences</router-link> |
+    <router-link to="/skills">Skills</router-link> |
+    <router-link to="/testimonials">Testimonials</router-link> |
+    <router-link to="/projects">Projects</router-link>
   </nav>
   <router-view/>
 </template>
